1a0d7676db9dec51dead67b41a821f0ca27299af
MarcoTagliafierro <tagliafierro.marco@gmail.com>
2020-12-03 16:09:16 +0100

    Added login filter

---
+++ DB2_Project_WEB/src/it/polimi/db2/project/filters/LoginFilter.java
@@ -0,0 +1,57 @@
+package it.polimi.db2.project.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class LoggedIn
+ */
+@WebFilter("/LoginFilter")
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() { }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() { }
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.print("Login checker filter executing ...\n");
+
+		// java.lang.String loginpath = "/index.html";
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		String loginpath = req.getServletContext().getContextPath() + "/LoginPage";
+
+		HttpSession s = req.getSession();
+		if (s.isNew() || s.getAttribute("user") == null) {
+			res.sendRedirect(loginpath);
+			return;
+		}
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException { }
+
+}
